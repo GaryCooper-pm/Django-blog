fe42d6046ea3fb38aeb424f6266bb9f3df7a0aeb
GaryCooper-pm <gary@pixelmixel.co.uk>
2022-07-21 14:44:28 +0000

    Update ALLOWED HOSTS in settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -32,7 +32,7 @@ DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ["mb2test.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["gc-codestar2022.herokuapp.com", "localhost"]
 
 
 # Application definition
